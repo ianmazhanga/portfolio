accf251d720c7f86a3e5d547a4b4cd9015d72f60
Ian <ianmazhanga@gmail.com>
2025-12-31 15:16:36 +0200

    Fix router basename for GitHub Pages deployment

--- App/src/components/App.tsx
+++ App/src/components/App.tsx
@@ -8,7 +8,7 @@ import Contact from '../pages/Contact/Contact';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/portfolio">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
